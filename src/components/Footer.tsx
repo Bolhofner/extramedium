efee6c2a78f5ef9c362f64317ce50c07e1327830
Bolhofner <brianbolhofner@gmail.com>
2020-11-19 22:31:03 -0600

    fix broken footer link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer>
       <div className="container">
-        <NavLink to="/#/" className="logo-font">conduit</NavLink>
+        <NavLink to="/" className="logo-font">conduit</NavLink>
         <span className="attribution">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
